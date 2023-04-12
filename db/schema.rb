7bb5249929f2090134af1fc3706aba172704da1c
Muhammad Zeeshan Shafqat <zeeshanshafqat1@outlook.com>
2023-04-13 08:39:48 +1000

    added published at to support post scheduling

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_10_222922) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_12_212143) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_10_222922) do
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
